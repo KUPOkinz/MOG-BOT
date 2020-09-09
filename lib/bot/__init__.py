941dec81d7e7e72a84dfebdc951a0f837a1491a1
KUPOkinz <63320687+KUPOkinz@users.noreply.github.com>
2020-09-08 21:44:51 -0400

    0.0.2 Testing Embeds

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed, File
 from discord.ext.commands import Bot as BotBase
 
 PREFIX = "*"
@@ -34,6 +36,24 @@ class Bot(BotBase):
 			self.ready = True
 			self.guild = self.get_guild(752575907012673569)
 			print("bot ready!")
+
+			channel = self.get_channel(752575907805528147)
+			await channel.send("Now online!")
+
+			embed = Embed(title="Now online!", description="MOG is now online!", 
+						  colour=0xFF0000, timestamp=datetime.utcnow())
+			fields = [("Name", "Value", True),
+					  ("Another field", "This field is next to the other one.", True),
+					  ("A non-inline field", "This field will appear of it's own row.", False)]
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+			embed.set_author(name="MOG", icon_url=self.guild.icon_url)
+			embed.set_footer(text="This is a footer!")
+			embed.set_thumbnail(url=self.guild.icon_url)
+			embed.set_image(url=self.guild.icon_url)
+			await channel.send(embed=embed)
+
+			await channel.send(file=File("./data/images/profile.png"))
 
 		else:
 			print("bot reconnected!")
