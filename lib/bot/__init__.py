5a2b2b10401af4d872105a33758fdd2d893b6e7c
KUPOkinz <63320687+KUPOkinz@users.noreply.github.com>
2020-09-09 18:45:14 -0400

    0.0.9 Implemented Cooldown Timers

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -8,7 +8,8 @@ from discord import Embed, File
 from discord.errors import HTTPException, Forbidden
 from discord.ext.commands import Context
 from discord.ext.commands import Bot as BotBase
-from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument)
+from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument,
+								  CommandOnCooldown)
 
 from ..db import db
 
@@ -95,17 +96,21 @@ class Bot(BotBase):
 		elif isinstance(exc, MissingRequiredArgument):
 			await ctx.send("One or more required arguments are missing!")
 
-		elif isinstance(exc.original, HTTPException):
-			await ctx.send("Unable to send message!")
-
-		elif isinstance(exc.original, Forbidden):
-			await ctx.send("I do not have permission to do that!")
+		elif isinstance(exc, CommandOnCooldown):
+			await ctx.send(f"That command is on {str(exc.cooldown.type).split('.')[-1]} cooldown. Try again in {exc.retry_after:,.2f} seconds!")
 
 		elif hasattr(exc, "original"):
-			raise exc.original
+			# if isinstance(exc.original, HTTPException):
+			#	await ctx.send("Unable to send message!")
 
-		else:
-			raise exc.original
+			if isinstance(exc.original, Forbidden):
+				await ctx.send("I do not have permission to do that!")
+
+			else:
+				raise exc.original
+
+		else: 
+			raise exc		
 
 	async def on_ready(self):
 		if not self.ready:
